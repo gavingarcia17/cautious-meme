c4aa8d6d40fb040cf62c5d6869b5a6894dd88477
gavingarcia17 <gavingarcia@myyahoo.com>
2025-01-27 00:01:41 -0800

    Bind host to 0.0.0.0 and increase timeout values

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 10000;
@@ -11,6 +12,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const server = http.createServer(app);
+
+server.keepAliveTimeout = 120000; // 120 seconds
+server.headersTimeout = 120000; // 120 seconds
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
