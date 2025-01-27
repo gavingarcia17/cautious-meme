e1fb88bff94a781ed5399ef67ceda4cb06ff6d3a
gavingarcia17 <gavingarcia@myyahoo.com>
2025-01-27 08:59:56 -0800

    Revert server listen and timeout values to original

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -5,8 +5,30 @@ const http = require('http');
 const app = express();
 const PORT = process.env.PORT || 5000; // Default to 5000 if PORT is not set
 
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Middleware to handle 403 errors
+app.use((req, res, next) => {
+  const err = new Error('Forbidden');
+  err.status = 403;
+  next(err);
+});
+
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
@@ -14,9 +36,6 @@ app.get('*', (req, res) => {
 
 const server = http.createServer(app);
 
-server.keepAliveTimeout = 120000; // 120 seconds
-server.headersTimeout = 120000; // 120 seconds
-
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
